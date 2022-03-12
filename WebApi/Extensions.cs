1e5cd4c006856b995eac1d2183cffd6c6c563de5
DaneVinson <dane.vinson@gmail.com>
2022-03-11 22:09:22 -0800

    refactors

--- WebApi/Extensions.cs
+++ WebApi/Extensions.cs
@@ -4,22 +4,25 @@ public static class Extensions
 {
     public static void MapCalculatorEndpoints(this WebApplication app)
     {
-        app.MapGet("/", (AppOptions options) => Results.Ok($"{options.Greeting} {options.Person}, welcome to the silly calculator API"));
+        app.MapGet(
+            "/", 
+            (AppOptions options) => 
+                        Results.Ok($"{options.Greeting} {options.Person}, welcome to the silly calculator API"));
         app.MapGet(
             "/{a}/add/{b}", 
-            ([FromServices] ICalculationHandler handler, decimal a, decimal b) => 
+            (ICalculationHandler handler, decimal a, decimal b) => 
                         handler.HandleAdd(a, b).GetResult());
         app.MapGet(
             "/{a}/subtract/{b}", 
-            ([FromServices] ICalculationHandler handler, decimal a, decimal b) => 
+            (ICalculationHandler handler, decimal a, decimal b) => 
                         handler.HandleSubtract(a, b).GetResult());
         app.MapGet(
             "/{a}/multiply/{b}", 
-            ([FromServices] ICalculationHandler handler, decimal a, decimal b) => 
+            (ICalculationHandler handler, decimal a, decimal b) => 
                         handler.HandleMultiply(a, b).GetResult());
         app.MapGet(
             "/{a}/divide/{b}", 
-            ([FromServices] ICalculationHandler handler, decimal a, decimal b) => 
+            (ICalculationHandler handler, decimal a, decimal b) => 
                         handler.HandleDivide(a, b).GetResult());
     }
     
